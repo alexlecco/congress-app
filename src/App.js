92ee89fd3de7cda8b6f50a48fd48294380d98ff3
Alex Villecco <alexlecco24@gmail.com>
2020-09-25 18:39:52 -0300

    create Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <p>Congress App</p>
+        <Footer />
       </header>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return <div className="headerContainer">Header</div>;
+}
+
+export default Header;
