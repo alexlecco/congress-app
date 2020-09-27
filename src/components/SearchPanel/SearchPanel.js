0b64a320f82bd0561c56435a2f93f5086ed299c1
Alex Villecco <alexlecco24@gmail.com>
2020-09-27 16:05:45 -0300

    make page responsive

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -5,19 +5,27 @@ function SearchPanel({ term, handleUpdate, isChecked, handleCheck }) {
   return (
     <div className="searchPanelContainer">
       <div className="search">
-        <div className="search__label">Search</div>
-        <input
-          className="search__input"
-          type="text"
-          value={term}
-          onChange={handleUpdate}
-        />
-        <div className="switchContainer">
-          <p className="switchLabel">Advanced Search</p>
-          <label className="switch">
-            <input type="checkbox" checked={isChecked} onChange={handleCheck} />
-            <span className="slider round"></span>
-          </label>
+        <div className="search__left">
+          <div className="search__left-label">Search</div>
+          <input
+            className="search__left-input"
+            type="text"
+            value={term}
+            onChange={handleUpdate}
+          />
+        </div>
+        <div className="search__right">
+          <div className="switchContainer">
+            <p className="switchLabel">Advanced Search</p>
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={handleCheck}
+              />
+              <span className="slider round"></span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
